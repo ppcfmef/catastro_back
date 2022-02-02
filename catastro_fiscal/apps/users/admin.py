89a39e77a308209577b3764be8f120674a97a426
Jose Ramirez <jcramireztello@gmail.com>
2022-02-01 19:20:54 -0500

    feat: add custom admin for user app

--- catastro_fiscal/apps/users/admin.py
+++ catastro_fiscal/apps/users/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Role
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser'),
-        }),
+        (_('Personal info'), {'fields': ('institution', 'dni', 'first_name', 'last_name', 'email', 'job_title')}),
+        (_('User account'), {'fields': ('role', 'username', 'password', 'is_active')}),
+        (_('User scope'), {'fields': ('department', 'province', 'district')}),
+        (_('Detail account'), {'fields': ('observation',)}),
     )
