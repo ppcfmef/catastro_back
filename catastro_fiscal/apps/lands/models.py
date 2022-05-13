f571a0bd7c15e2e36177df217207a2bfc53e61ec
Jose Ramirez <jcramireztello@gmail.com>
2022-05-13 05:59:04 -0500

    feat: inser new record owner and land

--- catastro_fiscal/apps/lands/models.py
+++ catastro_fiscal/apps/lands/models.py
@@ -65,3 +65,47 @@ class TemploralUploadRecord(models.Model):
 
     def __str__(self):
         return f'{self.id}'
+
+
+class LandOwner(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=50, blank=True, null=True)
+    document_type = models.CharField(max_length=2, blank=True, null=True)
+    dni = models.CharField(max_length=20)
+    name = models.CharField(max_length=150)
+    paternal_surname = models.CharField(max_length=150, blank=True, null=True)
+    maternal_surname = models.CharField(max_length=150, blank=True, null=True)
+    tax_address = models.CharField(max_length=255, blank=True, null=True)
+    numberLands = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'PROPIETARIO'
+        verbose_name = _('land owner')
+        verbose_name_plural = _('land owners')
+
+
+class Land(models.Model):
+    id = models.AutoField(primary_key=True)
+    cup = models.CharField(max_length=50)
+    cpm = models.CharField(max_length=50, blank=True, null=True)
+    habilitacion_name = models.CharField(max_length=150, blank=True, null=True)
+    steet_name = models.CharField(max_length=150, blank=True, null=True)
+    urban_mza = models.CharField(max_length=50, blank=True, null=True)
+    urban_lot_number = models.CharField(max_length=150, blank=True, null=True)
+    road_block_number = models.IntegerField(blank=True, null=True)
+    site = models.IntegerField(blank=True, null=True)
+    municipal_number = models.IntegerField(blank=True, null=True)
+    alternate_number = models.IntegerField(blank=True, null=True)
+    dpto_number = models.IntegerField(blank=True, null=True)
+    indoor = models.CharField(max_length=50, blank=True, null=True)
+    block = models.CharField(max_length=150, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    land_area = models.FloatField(blank=True, null=True)
+    built_area = models.FloatField(blank=True, null=True)
+    owner = models.ForeignKey(LandOwner, models.SET_NULL, blank=True, null=True)
+
+    class Meta:
+        db_table = 'PREDIO'
+        verbose_name = _('land')
+        verbose_name_plural = _('land')
--- catastro_fiscal/apps/lands/services.py
+++ catastro_fiscal/apps/lands/services.py
@@ -1,5 +1,5 @@
 import openpyxl
-from .models import UploadHistory, TemploralUploadRecord
+from .models import UploadHistory, TemploralUploadRecord, Land, LandOwner
 
 
 class ReadXlsService:
@@ -35,10 +35,60 @@ class UploadLandRecordService:
     def read(self, upload_history):
         return self.read_file_service(file=upload_history.file_upload.file).read()
 
-    def execute(self, upload_history: UploadHistory):
-        records = self.read(upload_history)
+    def temporal_upload(self, upload_history, records):
         temploral_upload_record_bulk = []
         for record in records:
             tmp_upload_record = TemploralUploadRecord(record=record, upload_history=upload_history, status='OK_NEW')
             temploral_upload_record_bulk.append(tmp_upload_record)
         TemploralUploadRecord.objects.bulk_create(temploral_upload_record_bulk)
+
+    def land_upload(self, upload_history):
+        temploral_records = TemploralUploadRecord.objects.filter(upload_history=upload_history, status='OK_NEW')
+        self.land_owner_upload(temploral_records)
+        land_bulk = []
+        for temploral in temploral_records:
+            record = temploral.record
+            land = Land(
+                cup=record.get('cod_pre'),
+                cpm=record.get('cod_cpu'),
+                habilitacion_name=record.get('nom_uu'),
+                steet_name=record.get('nom_via'),
+                urban_mza=record.get('mzn_urb'),
+                urban_lot_number=record.get('lot_urb'),
+                road_block_number=None,
+                site=None,
+                municipal_number=None,
+                alternate_number=record.get('km'),
+                dpto_number=None,
+                indoor=record.get('interior'),
+                block=record.get('block'),
+                latitude=record.get('coor_y'),
+                longitude=record.get('coor_x'),
+                land_area=record.get('area_terreno'),
+                built_area=None,
+                owner=LandOwner.objects.filter(dni=record.get('doc_iden')).first()
+            )
+            land_bulk.append(land)
+        Land.objects.bulk_create(land_bulk)
+
+    def land_owner_upload(self, temploral_records):
+        land_owners_bulk = []
+        for temploral in temploral_records:
+            record = temploral.record
+            land_owner = LandOwner(
+                code=record.get('cod_contr'),
+                document_type=record.get('tip_doc'),
+                dni=record.get('doc_iden'),
+                name=record.get('nombre'),
+                paternal_surname=record.get('ap_pat'),
+                maternal_surname=record.get('ap_mat'),
+                tax_address=record.get('dir_fiscal'),
+            )
+            land_owners_bulk.append(land_owner)
+        LandOwner.objects.bulk_create(land_owners_bulk)
+
+    def execute(self, upload_history: UploadHistory):
+        records = self.read(upload_history)
+        self.temporal_upload(upload_history, records)
+        upload_history = UploadHistory.objects.get(pk=33)
+        self.land_upload(upload_history)
