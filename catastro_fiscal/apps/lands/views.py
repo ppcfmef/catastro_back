0dc1ed4f52c71061c6252e8f6a8ac5c3e8546313
Jose Ramirez <jcramireztello@gmail.com>
2022-11-08 07:13:47 -0500

    feat: filter land by status
    task: T009

--- catastro_fiscal/apps/lands/views.py
+++ catastro_fiscal/apps/lands/views.py
@@ -40,7 +40,7 @@ class LandViewSet(mixins.ListModelMixin, GenericViewSet):
     serializer_class = LandSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, CamelCaseOrderFilter]
     search_fields = ['cup', 'cpm', 'id_cartographic_img', 'id_plot', 'street_name']
-    filterset_fields = ['owner', ]
+    filterset_fields = ['owner', 'status']
     ordering_fields = ['ubigeo', 'cup', 'cpm', 'id_plot', 'id_cartographic_img', 'habilitacion_name', 'street_name',
                        'creation_date']
     ordering = ['-creation_date']
