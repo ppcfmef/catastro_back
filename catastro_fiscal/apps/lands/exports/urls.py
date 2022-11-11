cf7fc348f2ae7ea07867a85152f435db3f0f2d23
Jose Carlos Gomez <jcgmarquez.0@gmail.com>
2022-11-11 12:08:21 -0500

    feat: create temporal upload records report

--- catastro_fiscal/apps/lands/exports/urls.py
+++ catastro_fiscal/apps/lands/exports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views.lands import LandExportView
+from .views.temporal_upload_records import TemporalUploadRecordExportView
 
 app_name = 'export_land'
 
 urlpatterns = [
-    path('records/', LandExportView.as_view())
+    path('records/', LandExportView.as_view()),
+    path('uploads/<int:upload_history>/', TemporalUploadRecordExportView.as_view()),
 ]
