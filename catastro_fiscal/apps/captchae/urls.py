ae59d20805b8fdec2d7715f4f5b20d54a1540485
FRANK SOTO PEÑA <franksotogithub>
2022-11-13 20:43:16 -0500

    agrengando url

--- catastro_fiscal/apps/captchae/urls.py
+++ catastro_fiscal/apps/captchae/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'captchae'
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
     path('api/', views.RestCaptchaView.as_view(), name='rest_captcha-view'),
 ]
