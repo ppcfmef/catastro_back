d5ce56989924c8e6a8f99d68021cfee20a855337
Jose Ramirez <jcramireztello@gmail.com>
2022-11-14 07:49:38 -0500

    feat: integrate custom admin tool

--- catastro_fiscal/catastro_fiscal/settings/base.py
+++ catastro_fiscal/catastro_fiscal/settings/base.py
@@ -28,6 +28,8 @@ if os.path.exists(BASE_DIR / '.env'):
 # Application definition
 
 DJANGO_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
