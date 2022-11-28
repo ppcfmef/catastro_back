5cf7b13d9ce961269eb0f81a11c7e8766525b601
Jose Ramirez <jcramireztello@gmail.com>
2022-11-28 03:15:33 -0500

    feat: add subpath deploy enviroment

--- catastro_fiscal/catastro_fiscal/urls.py
+++ catastro_fiscal/catastro_fiscal/urls.py
@@ -14,22 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf.urls.static import static
-from django.shortcuts import redirect
-from .swagger import schema_view
-
 urlpatterns = [
-    re_path('^$', lambda request: redirect('swagger/', permanent=False)),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('catastro_fiscal.urls_api', namespace='api')),
-    #path('captcha/', include('captcha.urls')),
-    path('captchae/', include('apps.captchae.urls')), #capthca urls
-    path('api/captcha/', include('rest_captcha.urls')),
-    path('export/', include('catastro_fiscal.urls_export', namespace='export')),
+    path(f'{getattr(settings, "DEPLOY_URL_PATH", "")}',
+         include('catastro_fiscal.urls_base', namespace='base')),
 ]
 
 if settings.DEBUG:
---
+++ catastro_fiscal/catastro_fiscal/urls_base.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.shortcuts import redirect
+from .swagger import schema_view
+
+app_name = 'base'
+
+urlpatterns = [
+    re_path('^$', lambda request: redirect('swagger/', permanent=False)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('catastro_fiscal.urls_api', namespace='api')),
+    path('captchae/', include('apps.captchae.urls')), #capthca urls
+    path('api/captcha/', include('rest_captcha.urls')),
+    path('export/', include('catastro_fiscal.urls_export', namespace='export')),
+]
