e50554e9247b50213ce4b00e2e3c4935e076f14f
Jose Ramirez <jose.ramirez@intelica.com>
2023-03-09 16:53:57 -0500

    feat: add test for api user list
    - refactor user test data
    - refactor url name for reverse

--- catastro_fiscal/apps/users/tests/__init__.py
+++ catastro_fiscal/apps/users/tests/__init__.py
@@ -1,2 +1,3 @@
 from .test_model_user import UserModelScopeTest, UserModelUbigeoTest  # noqa: F401
 from .test_api_sign_in import ApiSignInTest  # noqa: F401
+from .test_api_user import ApiUserTest  # noqa: F401
--- catastro_fiscal/apps/users/tests/test_model_user.py
+++ catastro_fiscal/apps/users/tests/test_model_user.py
@@ -1,52 +1,6 @@
 from django.test import TestCase
-from ..models import *
-
-
-class UserSetUpTestData:
-    NATIONAL_SCOPE = 1
-    DEPARTMENT_SCOPE = 2
-    PROVINCE_SCOPE = 3
-    DISTRICT_SCOPE = 4
-
-    @staticmethod
-    def mock():
-        User.objects.create_user(
-            dni='99999001',
-            username='99999001',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Nacional',
-        )
-
-        User.objects.create_user(
-            dni='99999002',
-            username='99999002',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Departamental',
-            department_id='15',
-        )
-
-        User.objects.create_user(
-            dni='99999003',
-            username='99999003',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501'
-        )
-
-        User.objects.create_user(
-            dni='99999004',
-            username='99999004',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501',
-            district_id='150101'
-        )
+from ..models import User
+from .user_setup_test_data import UserSetUpTestData
 
 
 class UserModelScopeTest(TestCase):
