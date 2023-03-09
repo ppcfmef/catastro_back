431bc0bf20a551f4ad100818f25ce8a7538da39f
Jose Ramirez <jcramireztello@gmail.com>
2023-03-09 05:28:19 -0500

    feat: add coverage tool
    - add pipenv dev library for coverage
    - refactor user test file

--- catastro_fiscal/apps/users/tests/user_model.py
+++ catastro_fiscal/apps/users/tests/user_model.py
@@ -1,10 +1,52 @@
 from django.test import TestCase
-from .models import *
+from ..models import *
 
-NATIONAL_SCOPE = 1
-DEPARTMENT_SCOPE = 2
-PROVINCE_SCOPE = 3
-DISTRICT_SCOPE = 4
+
+class UserSetUpTestData:
+    NATIONAL_SCOPE = 1
+    DEPARTMENT_SCOPE = 2
+    PROVINCE_SCOPE = 3
+    DISTRICT_SCOPE = 4
+
+    @staticmethod
+    def mock():
+        User.objects.create_user(
+            dni='99999001',
+            username='99999001',
+            password='prueba',
+            first_name='Usuario',
+            last_name='Nacional',
+        )
+
+        User.objects.create_user(
+            dni='99999002',
+            username='99999002',
+            password='prueba',
+            first_name='Usuario',
+            last_name='Departamental',
+            department_id='15',
+        )
+
+        User.objects.create_user(
+            dni='99999003',
+            username='99999003',
+            password='prueba',
+            first_name='Usuario',
+            last_name='Distrital',
+            department_id='15',
+            province_id='1501'
+        )
+
+        User.objects.create_user(
+            dni='99999004',
+            username='99999004',
+            password='prueba',
+            first_name='Usuario',
+            last_name='Distrital',
+            department_id='15',
+            province_id='1501',
+            district_id='150101'
+        )
 
 
 class UserModelScopeTest(TestCase):
@@ -18,59 +60,23 @@ class UserModelScopeTest(TestCase):
         """
         User Data for all test
         """
-        User.objects.create_user(
-            dni='99999001',
-            username='99999001',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Nacional',
-        )
-
-        User.objects.create_user(
-            dni='99999002',
-            username='99999002',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Departamental',
-            department_id='15',
-        )
-
-        User.objects.create_user(
-            dni='99999003',
-            username='99999003',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501'
-        )
-
-        User.objects.create_user(
-            dni='99999004',
-            username='99999004',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501',
-            district_id='150101'
-        )
+        UserSetUpTestData.mock()
 
     def test_national_scope(self):
         test_user_national = User.objects.get(username='99999001')
-        self.assertEquals(test_user_national.place_scope_id, NATIONAL_SCOPE)
+        self.assertEquals(test_user_national.place_scope_id, UserSetUpTestData.NATIONAL_SCOPE)
 
     def test_department_scope(self):
         test_user_department = User.objects.get(username='99999002')
-        self.assertEquals(test_user_department.place_scope_id, DEPARTMENT_SCOPE)
+        self.assertEquals(test_user_department.place_scope_id, UserSetUpTestData.DEPARTMENT_SCOPE)
 
     def test_province_scope(self):
         test_user_province = User.objects.get(username='99999003')
-        self.assertEquals(test_user_province.place_scope_id, PROVINCE_SCOPE)
+        self.assertEquals(test_user_province.place_scope_id, UserSetUpTestData.PROVINCE_SCOPE)
 
     def test_district_scope(self):
         test_user_district = User.objects.get(username='99999004')
-        self.assertEquals(test_user_district.place_scope_id, DISTRICT_SCOPE)
+        self.assertEquals(test_user_district.place_scope_id, UserSetUpTestData.DISTRICT_SCOPE)
 
 
 class UserModelUbigeoTest(TestCase):
@@ -84,43 +90,7 @@ class UserModelUbigeoTest(TestCase):
         """
         User Data for all test
         """
-        User.objects.create_user(
-            dni='99999001',
-            username='99999001',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Nacional',
-        )
-
-        User.objects.create_user(
-            dni='99999002',
-            username='99999002',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Departamental',
-            department_id='15',
-        )
-
-        User.objects.create_user(
-            dni='99999003',
-            username='99999003',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501'
-        )
-
-        User.objects.create_user(
-            dni='99999004',
-            username='99999004',
-            password='prueba',
-            first_name='Usuario',
-            last_name='Distrital',
-            department_id='15',
-            province_id='1501',
-            district_id='150101'
-        )
+        UserSetUpTestData.mock()
 
     def test_national_ubigeo(self):
         test_user_national = User.objects.get(username='99999001')
