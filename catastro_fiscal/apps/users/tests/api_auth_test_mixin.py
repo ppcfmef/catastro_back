c866e47167fc6fadf250b22cf2e9ed6ad2e9ac86
Jose Ramirez <jcramireztello@gmail.com>
2023-03-20 06:48:46 -0500

    test: valid api auth session
    - create custom mixin for auth api

---
+++ catastro_fiscal/apps/users/tests/api_auth_test_mixin.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import authenticate
+from rest_framework_jwt.settings import api_settings
+from ..models import User
+
+
+class APIAuthTestMixin:
+
+    def jwt(self):
+        self.jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        self.jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        self.jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
+        self.jwt_get_username_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
+
+    def auth(self):
+        """
+        User Auth
+        """
+        self.jwt()
+
+        # Create and signin user
+        self.test_user = User.objects.create_user(
+            dni='99999000',
+            username='admin',
+            password='password',
+            first_name='Admin',
+            last_name='Nacional',
+        )
+
+        user = authenticate(username='admin', password='password')
+        payload = self.jwt_payload_handler(user)
+
+        self.user_session = {
+            'token': self.jwt_encode_handler(payload),
+            'user': user
+        }
