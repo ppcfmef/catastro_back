cf7993f934df75dcb5fe588f9d952a607a78f464
Jose Ramirez <jcramireztello@gmail.com>
2023-05-24 23:57:43 -0500

    feat: add mobile app in settings

--- catastro_fiscal/apps/mobile/apps.py
+++ catastro_fiscal/apps/mobile/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MobileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mobile'
+    name = 'apps.mobile'
--- catastro_fiscal/catastro_fiscal/settings/base.py
+++ catastro_fiscal/catastro_fiscal/settings/base.py
@@ -63,6 +63,7 @@ LOCAL_APPS = [
     'apps.captchae.apps.CaptchaExampleConfig',
     'apps.historical.apps.HistoricalConfig',
     'apps.maintenance.apps.MaintenanceConfig',
+    'apps.mobile.apps.MobileConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
