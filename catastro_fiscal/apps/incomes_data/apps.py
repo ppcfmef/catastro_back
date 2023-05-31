71115013ea1f69c0cf94b8b3bad8654fdcb9dcbb
Jose Ramirez <jcramireztello@gmail.com>
2023-05-31 01:48:03 -0500

    feat: add incomes data app
    - add 4 models

---
+++ catastro_fiscal/apps/incomes_data/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IncomesDataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.incomes_data'
--- catastro_fiscal/catastro_fiscal/settings/base.py
+++ catastro_fiscal/catastro_fiscal/settings/base.py
@@ -64,6 +64,7 @@ LOCAL_APPS = [
     'apps.historical.apps.HistoricalConfig',
     'apps.maintenance.apps.MaintenanceConfig',
     'apps.mobile.apps.MobileConfig',
+    'apps.incomes_data.apps.IncomesDataConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
