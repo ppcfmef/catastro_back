14633b8860e409d09a785764a47945c2961469db
Jose Ramirez <jcramireztello@gmail.com>
2023-06-08 12:10:30 -0500

    feat: add income data task and api

--- catastro_fiscal/apps/incomes_data/serializers.py
+++ catastro_fiscal/apps/incomes_data/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from apps.lands.models import UploadHistory
 from .services.rt_contribuyente_upload_temporal import RTContribuyenteUploadTemporalService
+from .tasks import process_incomes_upload_tenporal, process_incomes_upload
+from .models import Contribuyente, MarcoPredio, Arancel, PredioDato
 
 
 class IncomeUploadHistorySerializer(serializers.ModelSerializer):
@@ -16,14 +18,50 @@ class IncomeUploadHistorySerializer(serializers.ModelSerializer):
         })
         instance = super(IncomeUploadHistorySerializer, self).create(validated_data)
         upload_temporal_service = self.fabric_temporal_service(instance.type_upload)
-        self.load_file_upload(upload_temporal_service, instance)
+        self.load_file_upload(instance)
         return upload_temporal_service().get_temporal_summary(instance)
 
-    def load_file_upload(self, upload_temporal_service, instance):
-        upload_temporal_service().execute(instance)
+    def load_file_upload(self, instance):
+        process_incomes_upload_tenporal.send(instance.id)
 
     def fabric_temporal_service(self, type_upload):
         if type_upload == 'RT_CONTRIBUYENTE':
             return RTContribuyenteUploadTemporalService
         else:
             raise serializers.ValidationError('No existe tipo de carga para procesar')
+
+
+class IncomeUploadStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadHistory
+        fields = ('status', )
+
+    def update(self, instance, validated_data):
+        instance = super(IncomeUploadStatusSerializer, self).update(instance, validated_data)
+        if instance.status == 'IN_PROGRESS':
+            process_incomes_upload.send(instance.id)
+        return instance
+
+
+class RTContribuyenteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contribuyente
+        fields = '__all__'
+
+
+class RTMarcoPredioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MarcoPredio
+        fields = '__all__'
+
+
+class RTArancelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Arancel
+        fields = '__all__'
+
+
+class RTPredioDatoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PredioDato
+        fields = '__all__'
--- catastro_fiscal/apps/incomes_data/urls.py
+++ catastro_fiscal/apps/incomes_data/urls.py
@@ -1,9 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from .views import IncomeUploadHistoryViewset
+from .views import (
+    IncomeUploadHistoryViewset, IncomeUploadStatusViewSet, RTContribuyenteViewSet, RTMarcoPredioViewSet,
+    RTArancelViewSet, RTPredioDatoViewSet
+)
 
 app_name = 'api_incomes'
 
 router = DefaultRouter()
 router.register('upload', IncomeUploadHistoryViewset)
+router.register('upload/status', IncomeUploadStatusViewSet)
+router.register('rtcontribuyente', RTContribuyenteViewSet)
+router.register('rtmarcopredio', RTMarcoPredioViewSet)
+router.register('rtarancel', RTArancelViewSet)
+router.register('rtpediodato', RTPredioDatoViewSet)
 
 urlpatterns = router.urls
